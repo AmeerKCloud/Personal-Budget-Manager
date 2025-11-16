28caa2dd1c14311a7aec94bb489af174e5d46bdc
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-11-16 12:03:57 -0500

    Update class_files.py

--- src/modules/class_files.py
+++ src/modules/class_files.py
@@ -1,6 +1,7 @@
 
 
 class Transaction:
+    """Stores type (“income” or “expense”), description, and amount."""
     def __init__(self, description, amount):
         self.description = description
         self.amount = amount
@@ -8,6 +9,8 @@ class Transaction:
         self.expense = {}
 
 
-
+class Budget:
+    """Stores total available money.
+    Stores all transactions in a list."""
 
 
