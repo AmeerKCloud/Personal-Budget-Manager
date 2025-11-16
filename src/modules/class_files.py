9bc0fc10805fe6f8e9b1fb5cc05fa933b0434cc9
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-11-16 13:11:13 -0500

    Update class_files.py

--- src/modules/class_files.py
+++ src/modules/class_files.py
@@ -8,6 +8,7 @@ class Transaction:
         self.amount = amount            #⬅️ Let this be a integer
 
     def transac_data(self):
+        transac_dict = {} 
         transac_dict[self.type] = {}     #⬅️ Currently here, figuring out how to add user inputs as key-values
         transac_dict[self.type]["description"] = self.description
         transac_dict[self.type]["amount"] = self.amount
