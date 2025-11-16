539e7ae4df5a4340f3442b032da532fe71931757
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-11-16 12:47:45 -0500

    Update class_files.py

--- src/modules/class_files.py
+++ src/modules/class_files.py
@@ -3,12 +3,14 @@
 class Transaction:
     """Stores type (“income” or “expense”), description, and amount."""
     def __init__(self, type, description, amount):
+        self.type = type
         self.description = description
         self.amount = amount            #⬅️ Let this be a integer
-        self.type = type
 
     def transac_data(self):
-        pass
+        transac_dict = {}
+        transac_dict[self.type] = [self.description, self.amount]
+        print(transac_dict)
 
 
 class Budget:
@@ -21,5 +23,6 @@ class Budget:
         total_available_money = 0
         pass
 
+transac_type = input("Enter your transaction type: 'income' or 'expense'? ").lower()
 
-
+transaction = transaction(transac_type, transac_description, transac_amount)
