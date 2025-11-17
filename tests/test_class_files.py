4220013ff0426f91ababbe7f947df1ea7924e952
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-11-17 16:52:34 -0500

    Update test_class_files.py

--- tests/test_class_files.py
+++ tests/test_class_files.py
@@ -1,57 +1,57 @@
 # Here go all of the test-class code.
 
-# class Transaction:
-#     def __init__(self, type, date, description, amount):
-#         self.type = type
-#         self.date = date
-#         self.description = description
-#         self.amount = amount            #⬅️ Let this be a integer
+class Transaction:
+    def __init__(self, type, date, description, amount):
+        self.type = type
+        self.date = date
+        self.description = description
+        self.amount = amount            #⬅️ Let this be a integer
 
-#     def transac_data(self):
-#         """Stores type (“income” or “expense”), 
-#         description, and amount."""
+    def transac_data(self):
+        """Stores type (“income” or “expense”), 
+        description, and amount."""
 
-#         transac_dict = {} 
-#         transac_dict[self.type] = {}
-#         transac_dict[self.type]["date"] = self.date
-#         transac_dict[self.type]["description"] = self.description
-#         transac_dict[self.type]["amount"] = self.amount
-#         # print(transac_dict)
-#         return transac_dict
+        transac_dict = {} 
+        transac_dict[self.type] = {}
+        transac_dict[self.type]["date"] = self.date
+        transac_dict[self.type]["description"] = self.description
+        transac_dict[self.type]["amount"] = self.amount
+        # print(transac_dict)
+        return transac_dict
 
 
-# class Budget:
-#     """Stores total available money.
-#     Stores all transactions in a list."""
+class Budget:
+    """Stores total available money.
+    Stores all transactions in a list."""
 
-#     def __init__(self, money, transaction):
-#         self.money = money
-#         all_transactions = []
-#         total_available_money = 0
-#         pass
+    def __init__(self, money, transaction):
+        self.money = money
+        all_transactions = []
+        total_available_money = 0
+        pass
 
-#     def add_income(self):
-#         """Returns the sum of all income transactions."""
-#         pass
+    def add_income(self):
+        """Returns the sum of all income transactions."""
+        pass
 
-#     def add_expense(self):
-#         """Returns the sum of all expense transactions."""
-#         pass
+    def add_expense(self):
+        """Returns the sum of all expense transactions."""
+        pass
 
-#     def calculate_balance(self):
-#         pass
+    def calculate_balance(self):
+        pass
 
-#     def show_transactions(self):
+    def show_transactions(self):
         
-#         pass
+        pass
 
-# transac_type = input("\nEnter your transaction type: 'income' or 'expense'? ").lower()
-# transac_date = input("Enter todays date: ")
-# transac_description = input("Briefly describe your transaction: ").capitalize()
-# transac_amount = int(input("Enter your transaction amount: $"))
+transac_type = input("\nEnter your transaction type: 'income' or 'expense'? ").lower()
+transac_date = input("Enter todays date: ")
+transac_description = input("Briefly describe your transaction: ").capitalize()
+transac_amount = int(input("Enter your transaction amount: $"))
 
-# transaction = Transaction(transac_type, transac_date, transac_description, transac_amount)
-# transac_dictionary = transaction.transac_data()
+transaction = Transaction(transac_type, transac_date, transac_description, transac_amount)
+transac_dictionary = transaction.transac_data()
 
 
 
