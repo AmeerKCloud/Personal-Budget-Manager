d54ac500688e00bdb7a7a1331867e422a0c6b189
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-11-17 13:04:54 -0500

    Update class_files.py

--- src/modules/class_files.py
+++ src/modules/class_files.py
@@ -1,57 +1,82 @@
 
 
-class Transaction:
-    def __init__(self, type, date, description, amount):
-        self.type = type
-        self.date = date
-        self.description = description
-        self.amount = amount            #⬅️ Let this be a integer
+# class Transaction:
+#     def __init__(self, type, date, description, amount):
+#         self.type = type
+#         self.date = date
+#         self.description = description
+#         self.amount = amount            #⬅️ Let this be a integer
 
-    def transac_data(self):
-        """Stores type (“income” or “expense”), 
-        description, and amount."""
+#     def transac_data(self):
+#         """Stores type (“income” or “expense”), 
+#         description, and amount."""
 
-        transac_dict = {} 
-        transac_dict[self.type] = {}
-        transac_dict[self.type]["date"] = self.date
-        transac_dict[self.type]["description"] = self.description
-        transac_dict[self.type]["amount"] = self.amount
-        # print(transac_dict)
-        return transac_dict
+#         transac_dict = {} 
+#         transac_dict[self.type] = {}
+#         transac_dict[self.type]["date"] = self.date
+#         transac_dict[self.type]["description"] = self.description
+#         transac_dict[self.type]["amount"] = self.amount
+#         # print(transac_dict)
+#         return transac_dict
 
 
-class Budget:
-    """Stores total available money.
-    Stores all transactions in a list."""
+# class Budget:
+#     """Stores total available money.
+#     Stores all transactions in a list."""
 
-    def __init__(self, money, transaction):
-        self.money = money
-        all_transactions = []
-        total_available_money = 0
-        pass
+#     def __init__(self, money, transaction):
+#         self.money = money
+#         all_transactions = []
+#         total_available_money = 0
+#         pass
 
-    def add_income(self):
-        """Returns the sum of all income transactions."""
-        pass
+#     def add_income(self):
+#         """Returns the sum of all income transactions."""
+#         pass
 
-    def add_expense(self):
-        """Returns the sum of all expense transactions."""
-        pass
+#     def add_expense(self):
+#         """Returns the sum of all expense transactions."""
+#         pass
 
-    def calculate_balance(self):
-        pass
+#     def calculate_balance(self):
+#         pass
 
-    def show_transactions(self):
+#     def show_transactions(self):
         
-        pass
+#         pass
 
-transac_type = input("\nEnter your transaction type: 'income' or 'expense'? ").lower()
-transac_date = input("Enter todays date: ")
-transac_description = input("Briefly describe your transaction: ").capitalize()
-transac_amount = int(input("Enter your transaction amount: $"))
+# transac_type = input("\nEnter your transaction type: 'income' or 'expense'? ").lower()
+# transac_date = input("Enter todays date: ")
+# transac_description = input("Briefly describe your transaction: ").capitalize()
+# transac_amount = int(input("Enter your transaction amount: $"))
 
-transaction = Transaction(transac_type, transac_date, transac_description, transac_amount)
-transac_dictionary = transaction.transac_data()
+# transaction = Transaction(transac_type, transac_date, transac_description, transac_amount)
+# transac_dictionary = transaction.transac_data()
 
-for key, value in transac_dictionary.items():
+
+transac_dictionary = {              #⬅️ This is a test dictionary to experiement and work our for-loop out with.
+    "income" : {
+        "date" : "11/17/2025",
+        "description" : "paycheck",
+        "amount" : 250,
+    },
+    "expense" : {
+        "date" : "10/20/2025",
+        "description" : "groceries",
+        "amount" : 100,
+    },
+    "income" : {
+        "date" : "09/10/2025",
+        "description" : "groceries",
+        "amount" : 100,
+    },
+    "expense" : {
+        "date" : "08/15/2025",
+        "description" : "groceries",
+        "amount" : 100,
+    },
+}
+
+
+for key, value in transac_dictionary.items():       #⬅️ For-loop to work out.
     print(f"{key}: {value}")
