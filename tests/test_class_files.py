e2a42a0d3060edce10b34bfcdcb17ae11063c490
Ameer Khan <ameer.khan.linkedin@gmail.com>
2025-11-26 13:44:02 -0500

    Update test_class_files.py

--- tests/test_class_files.py
+++ tests/test_class_files.py
@@ -50,15 +50,12 @@ class Budget:                           #⬅️Currently trying to figure out ho
         income_total = 0
         # income_total += self.all_transactions["income"]["amount"]
         for item in self.all_transactions["income"]:
-            if item == "amount":
-                income_total += item
-            else:
-                income_total = 0
+            income_total += item["amount"]
         print(f"Your total income: ${income_total}")
 
     def add_expense(self):
         """Returns the sum of all expense transactions."""
-        expense_total = 0
+        # expense_total = 0
         expense_total = sum(item.get("amount", 0) for item in self.all_transactions["expense"])
         print(f"Your total expenses: ${expense_total}")
 
