9bed5a4cb7ccd4ab34480b170921a433b26edc57
Ameer Khan <ameer.khan.linkedin@gmail.com>
2025-11-26 12:55:51 -0500

    Update test_class_files.py

--- tests/test_class_files.py
+++ tests/test_class_files.py
@@ -87,6 +87,7 @@ class Budget:                           #⬅️Currently trying to figure out ho
 #TODO: Describe the problem:
 # When the user chooses option '(d) View expense total' program prints the same amount as income
 # - Even though the user never entered an expense and so expense should be empty
+# - - To fix this, the 'add_expense/income' methods in 'Budget' class must be modified.
 
 
 
