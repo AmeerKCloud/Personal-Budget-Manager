ffaf521e05e2089030a44f261d090b618c2aa981
Ameer Khan <ameer.khan.linkedin@gmail.com>
2025-11-26 13:14:08 -0500

    Update test_class_files.py

--- tests/test_class_files.py
+++ tests/test_class_files.py
@@ -54,7 +54,8 @@ class Budget:                           #⬅️Currently trying to figure out ho
     def add_expense(self):
         """Returns the sum of all expense transactions."""
         expense_total = 0
-        expense_total += self.all_transactions["expense"]["amount"]
+        # expense_total += self.all_transactions["expense"]["amount"]
+        
         print(f"Your total expenses: ${expense_total}")
 
     def calculate_balance(self):
