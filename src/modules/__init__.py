664ff77482c1328be60bfa301852db80d9499ccb
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-11-27 09:52:20 -0500

    Update __init__.py

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -1,4 +1,8 @@
 # Purpose of the '__init__.py' file:
-
+# Contain an __init__.py file (even an empty one) in a directory
+# to mark it as a Python package, so that modules within the package
+# can be imported elsewhere in your code.
+# This file tells Python:
+# “Hey, this folder is a package. You’re allowed to import from here.”
 
 print("Initializing mypackage")
