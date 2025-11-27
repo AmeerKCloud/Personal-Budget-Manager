31832137d04ecb81aabf447352ad5b4911d31a26
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-11-27 10:15:10 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -11,18 +11,15 @@ while True:
     print("|")
     user_choice = input("Choose an option: \n(a) Add new transaction, \n(b) view all transactions, \n(c) view income total, \n(d) view expense total, \n(e) view balance, \n(f) exit:\n ").lower()
 
-    if user_choice == "a":
-        
-        transac_type = input("\nEnter transaction type: 'income' or 'expense'? ").lower()
-        transac_date = input("Enter transaction date: ")
-        transac_description = input("Briefly describe your transaction: ").capitalize()
-        transac_amount = float(input("Enter transaction amount: $"))
+    user_input = UserInputs()
 
-        transaction = Transaction(transac_type, transac_date, transac_description, transac_amount)    #⬅️ Object
+    if user_choice == "a":
+
+        transaction = Transaction(user_input.transac_type, user_input.transac_date, user_input.transac_description, user_input.transac_amount)    #⬅️ Object
         current_transac_data = transaction.current_transac_data()
 
         for key in current_transac_data:
-            if key == transac_type:
+            if key == user_input.transac_type:
                 for list_item in current_transac_data[key]:
                     transaction = list_item                     #⬅️ This is the dict. inside the list.
                 budget.add_transactions(transaction, key)
@@ -36,3 +33,44 @@ while True:
         budget.calculate_balance()
     else:
         break
+
+
+
+# _______ Reserve Code _______
+
+# from modules.other_helpers import UserInputs
+# from modules.class_files import Transaction, Budget
+
+# budget = Budget()   #⬅️ Object
+
+# while True:
+#     print("\n")
+#     print("\n----- Budget Tracker -----")
+#     print("|")
+#     user_choice = input("Choose an option: \n(a) Add new transaction, \n(b) view all transactions, \n(c) view income total, \n(d) view expense total, \n(e) view balance, \n(f) exit:\n ").lower()
+
+#     if user_choice == "a":
+        
+#         transac_type = input("\nEnter transaction type: 'income' or 'expense'? ").lower()
+#         transac_date = input("Enter transaction date: ")
+#         transac_description = input("Briefly describe your transaction: ").capitalize()
+#         transac_amount = float(input("Enter transaction amount: $"))
+
+#         transaction = Transaction(transac_type, transac_date, transac_description, transac_amount)    #⬅️ Object
+#         current_transac_data = transaction.current_transac_data()
+
+#         for key in current_transac_data:
+#             if key == transac_type:
+#                 for list_item in current_transac_data[key]:
+#                     transaction = list_item                     #⬅️ This is the dict. inside the list.
+#                 budget.add_transactions(transaction, key)
+#     elif user_choice == "b":    
+#         budget.show_transactions()
+#     elif user_choice == "c":
+#         budget.add_income()
+#     elif user_choice == "d":
+#         budget.add_expense()
+#     elif user_choice == "e":
+#         budget.calculate_balance()
+#     else:
+#         break
