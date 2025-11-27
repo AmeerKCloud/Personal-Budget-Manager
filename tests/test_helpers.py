aa993b5e2648a857d9db176418aa353e4576eabf
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-11-27 09:22:56 -0500

    Update test_helpers.py

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -3,9 +3,11 @@
 from test_class_files import Transaction
 
 class UserInputs:
+    """Handles all user inputs for transactions."""
     def __init__(self):
         self.transac_type = input("\nEnter transaction type: 'income' or 'expense'? ").lower()
         self.transac_date = input("Enter transaction date: ")
         self.transac_description = input("Briefly describe your transaction: ").capitalize()
         self.transac_amount = float(input("Enter transaction amount: $"))
     
+
