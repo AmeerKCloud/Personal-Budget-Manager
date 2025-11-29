143b9e205bc3f473d778e1c561bbf1e1345784c2
AmeerKCloud <ameer.khan.linkedin@gmail.com>
2025-11-29 12:14:51 -0500

    Update test_helpers.py

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -11,15 +11,16 @@ class UserInputs:
         self.transac_description = None
         self.transac_amount = None        #⬅️ Still gets added as 'None' if invalid input is given.
 
-        try:
-            self.transac_type = input("\nEnter transaction type: 'income' or 'expense'? ").lower()
-            self.transac_date = input("Enter transaction date: ")
-            self.transac_description = input("Briefly describe your transaction: ").capitalize()
-            self.transac_amount = float(input("Enter transaction amount: $"))
-        except ValueError:
-            print("❌ Invalid input. Please enter the correct data types. Transaction not created.")
-        except AttributeError:
-            print("Attribute error encountered. Please check your inputs.")  
+        while True:
+            try:
+                self.transac_type = input("\nEnter transaction type: 'income' or 'expense'? ").lower()
+                self.transac_date = input("Enter transaction date: ")
+                self.transac_description = input("Briefly describe your transaction: ").capitalize()
+                self.transac_amount = float(input("Enter transaction amount: $"))
+            except ValueError:
+                print("❌ Invalid input. Please enter the correct data types. Transaction not created.")
+            except AttributeError:
+                print("Attribute error encountered. Please check your inputs.")  
 
 
 # IMPORTANT:
