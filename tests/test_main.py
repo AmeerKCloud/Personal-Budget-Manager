d2d8ae80791c2464400b1d2619b9d6aabb2f32ee
Ameer Khan <ameer.khan.linkedin@gmail.com>
2025-12-04 19:15:26 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -65,7 +65,48 @@ while True:
 
 
 
-# _______ Reserve Code _______
+
+# _______ Reserve Code 2 _______
+
+
+# from test_helpers import UserInputs
+# from test_class_files import Transaction, Budget
+
+# budget = Budget()   #⬅️ Object
+
+# while True:
+#     print("\n----- Budget Tracker -----")
+#     print("|")
+#     user_choice = input("Choose an option: \n(a) Add new transaction, \n(b) view all transactions, \n(c) view income total, \n(d) view expense total, \n(e) view balance, \n(f) exit:\n ").lower()
+
+#     if user_choice == "a":
+#         user_input = UserInputs()   #⬅️ Object to gather user inputs for transaction details.
+#         transaction = Transaction(user_input.transac_input(), user_input.date_input(), user_input.description_input(), user_input.amount_input())    #⬅️ Object
+#         current_transac_data = transaction.current_transac_data()
+
+#         for key in current_transac_data:
+#             if key == user_input.transac_input():
+#                 for list_item in current_transac_data[key]:
+#                     transaction = list_item                     #⬅️ This is the dict. inside the list.
+#                 budget.add_transactions(transaction, key)
+#     elif user_choice == "b":    
+#         budget.show_transactions()
+#     elif user_choice == "c":
+#         budget.add_income()
+#     elif user_choice == "d":
+#         budget.add_expense()
+#     elif user_choice == "e":
+#         budget.calculate_balance()
+#     elif user_choice == "f":
+#         break
+#     else:
+#         print("Invalid option. Try again.")
+#         break
+
+
+
+
+# _______ Reserve Code 1 _______
 
 # from test_helpers import UserInputs
 # from test_class_files import Transaction, Budget
