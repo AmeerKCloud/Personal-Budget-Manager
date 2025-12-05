a8fe1ccac10212416fbc255566d7104078e688bd
Ameer Khan <ameer.khan.linkedin@gmail.com>
2025-12-04 19:19:19 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -18,11 +18,11 @@ while True:
 
     if user_choice == "a":
         user_input = UserInputs()   #⬅️ Object to gather user inputs for transaction details.
-        transaction = Transaction(user_input.transac_input(), user_input.date_input(), user_input.description_input(), user_input.amount_input())    #⬅️ Object
+        transaction = Transaction(user_input.transac_type, user_input.transac_date, user_input.transac_description, user_input.transac_amount)    #⬅️ Object
         current_transac_data = transaction.current_transac_data()
 
         for key in current_transac_data:
-            if key == user_input.transac_input():
+            if key == user_input.transac_type:
                 for list_item in current_transac_data[key]:
                     transaction = list_item                     #⬅️ This is the dict. inside the list.
                 budget.add_transactions(transaction, key)
